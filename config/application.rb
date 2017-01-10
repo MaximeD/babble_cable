b4766eb4d4dc747293fd04f394ee735ce8828450
Maxime Demolin <akbarova.armia@gmail.com>
2017-01-10 18:31:19 +0100

    ADD I18n
    Application should be localized in french.
    
    Details
    * ADD french as default locale
    * UPDATE locales load path

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,9 @@ module BabbleCable
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.available_locales = %w[fr]
+    config.i18n.default_locale = :fr
   end
 end
