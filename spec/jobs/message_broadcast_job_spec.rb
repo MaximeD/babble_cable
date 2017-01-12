0f5b6da4c03032fec4817e548585e0a044930e86
Maxime Demolin <akbarova.armia@gmail.com>
2017-01-12 18:27:20 +0100

    ADD persisted messages
    Messaages should be persisted, otherwise they will disappear after page
    has been reloaded.
    
    Details
    * ADD saving messages
    * ADD delayed broadcasting

---
+++ spec/jobs/message_broadcast_job_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe MessageBroadcastJob do
+  describe '.perform' do
+    let(:message) { Message.new(text: 'test', room_id: 1) }
+    let(:server) do
+      OpenStruct.new(broadcast: -> {}).tap do |server|
+        allow(server).to receive(:broadcast)
+      end
+    end
+
+    before { allow(ActionCable).to receive(:server) { server } }
+
+    subject! { described_class.perform_now(message) }
+
+    it 'broadcasts message' do
+      expect(server).to have_received(:broadcast)
+        .with 'room_channel-1', message: 'test'
+    end
+  end
+end
