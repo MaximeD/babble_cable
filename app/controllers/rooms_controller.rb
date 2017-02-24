335087765b2c2bf26c733bd5fe70b05cceaf0046
unplugandplay <unplugandplay@free.fr>
2017-02-24 20:58:03 +0100

    simplification du controller rooms

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,9 +1,8 @@
 class RoomsController < ApplicationController
-  before_action :redirect_to_existing_room, only: %i[create]
 
   # POST /rooms
   def create
-    @room = Room.new(room_params)
+    @room = Room.find_or_create_by(name: room_params[:name])
 
     if @room.save
       redirect_to room_messages_path(room_id: @room.id),
@@ -19,12 +18,4 @@ class RoomsController < ApplicationController
     params.require(:room).permit(:name)
   end
 
-  # Upon creation, if room already exists, redirect user to it.
-  def redirect_to_existing_room
-    room = Room.find_by(name: room_params[:name])
-    return unless room
-
-    redirect_to room_messages_path(room_id: room.id),
-      notice: t('.welcome_to_room', room: room.name)
-  end
 end
